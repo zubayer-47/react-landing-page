efdb0acf9b847733d28925d1e749af7ac18e54ac
Zubayer <zubayerabm47@gmail.com>
2021-06-14 11:19:47 +0600

    changed something new

--- src/Components/HomeComp/Home.jsx
+++ src/Components/HomeComp/Home.jsx
@@ -4,7 +4,7 @@ import './Home.css'
 const Home = () => {
     return (
         <div className='homeParent'>
-            <h3>Welcome</h3>
+            <h3>Hello</h3>
             <p>We Are The MERN Developer</p>
         </div>
     )
